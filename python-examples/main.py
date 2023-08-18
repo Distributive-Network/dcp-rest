4bfb9926919b6e2071f109ea3d5c95efa1c9e328
Will Pringle <will@distributive.network>
2023-08-17 23:31:49 -0400

    show python example

---
+++ python-examples/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import pdb
+import time
+import dcp
+
+workFun = dcp.WorkFunction("lambda x: x*5")
+data = [1,2,3]
+
+job = dcp.computeFor(data, workFun, "0x3d4393D4A143310C10A4C9Ff259B5d2DFbb2AA83", "1")
+
+while True:
+  time.sleep(5)
+  status = job.status()
+  print(status)
+  if (status['status'] in ("cancelled", "finished")):
+    break
+
+print("DCP job done! Here are the results!")
+print(job.result())
+
