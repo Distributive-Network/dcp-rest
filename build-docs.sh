2a4ef31eb8c9ae3ee2ae9273cea5283e2e3e2920
Will Pringle <will@distributive.network>
2023-11-23 10:33:14 -0500

    docs(website): add green colour to swagger api

--- build-docs.sh
+++ build-docs.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 npx @redocly/cli build-docs spec.yaml 
-mv redoc-static.html public/docs.html
+
+# change colour to match theme
+sed 's/#263238/#1aa473/g' redoc-static.html  > public/docs.html
+
+rm redoc-static.html
 
